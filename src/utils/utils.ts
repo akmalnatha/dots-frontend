5d3f1caac81018f42cefcdf4b9e7060b8f24b7d4
Akmal Natha Abhirama <akmalabhirama2002@gmail.com>
2024-10-13 16:10:40 +0700

    feat: function utils

---
+++ src/utils/utils.ts
@@ -0,0 +1,39 @@
+export async function hashAnswer(answer: string) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(answer);
+
+  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer)); // Convert buffer to byte array
+  const hashHex = hashArray
+    .map((byte) => byte.toString(16).padStart(2, "0"))
+    .join(""); // Convert bytes to hex string
+
+  return hashHex;
+}
+
+export function shuffleArray(array: string[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+export function decodeHtmlEntities(text: string) {
+  const element = document.createElement("div");
+  if (text) {
+    element.innerHTML = text;
+    return element.textContent || element.innerText || "";
+  }
+  return text;
+}
+
+export function formatTime (seconds: number) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const secs = seconds % 60;
+    return `${String(hours).padStart(2, "0")}:${String(minutes).padStart(
+      2,
+      "0"
+    )}:${String(secs).padStart(2, "0")}`;
+  };
