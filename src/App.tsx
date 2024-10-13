195e8c5ef7687525b61ff0347d3bc35b4b3e3144
Akmal Natha Abhirama <akmalabhirama2002@gmail.com>
2024-10-13 16:10:54 +0700

    feat: configure app and main

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
-import QuestionNumber from './components/question_number'
+import Quiz from './pages/quiz/page';
+import Home from './pages/home/page';
+import SignIn from './pages/sign-in/page';
+import SignUp from './pages/sign-up/page';
+import ProtectedRoute from './utils/protectedRoute';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <QuestionNumber number={1} currentNumber={1} answered={false}/>
-        <QuestionNumber number={2} currentNumber={1} answered={false}/>
-        <QuestionNumber number={3} currentNumber={1} answered={true}/>
-      </div>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* Main Page */}
+        <Route path="/sign-in" element={<SignIn/>} />
+        <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/" element={
+          <ProtectedRoute>
+            <Home/>
+          </ProtectedRoute>
+          } />
+        <Route path="/quiz" element={
+          <ProtectedRoute>
+            <Quiz/>
+          </ProtectedRoute>
+          } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ToastContainer } from "react-toastify";
+import { QuizProvider } from "./utils/quizContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   // <StrictMode>
-    <App />
+  <>
+    <QuizProvider>
+      <ToastContainer />
+      <App />
+    </QuizProvider>
+  </>
   // </StrictMode>
-)
+);
